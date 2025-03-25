759d005ae339bee3b6f59dfa68aa04f932aea741
oliviiagubska <lon24gubs01@mpw.ac.uk>
2025-03-25 13:20:57 +0000

    sessions

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import sqlite3
 import hashlib
+
 app = Flask(__name__)
+app.secret_key = 'random'
 
 con = sqlite3.connect("login.db")
 cur = con.cursor()
@@ -40,9 +42,19 @@ def login():
         user = cur.fetchone()
         print(user)
         if user:
-            return "login success"
+            session["username"] = request.form["username"]
+            return render_template("welcome.html")
         else:
             return "login failed"
+           
+@app.route("/w")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
